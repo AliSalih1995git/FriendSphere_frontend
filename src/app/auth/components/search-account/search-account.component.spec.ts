3bd262444c5a562a9462ab93c1e697cc0df08ba1
AliSalih1995git <kvaliswalihtnl@gmail.com>
2023-08-20 09:57:12 +0530

    Added Cypress e2e tests for login component,and Unit test

--- src/app/auth/components/search-account/search-account.component.spec.ts
+++ src/app/auth/components/search-account/search-account.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchAccountComponent } from './search-account.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AuthService } from '../../service/auth.service';
+import { of } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
 
 describe('SearchAccountComponent', () => {
   let component: SearchAccountComponent;
@@ -8,9 +12,10 @@ describe('SearchAccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchAccountComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      declarations: [SearchAccountComponent],
+      providers: [AuthService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchAccountComponent);
     component = fixture.componentInstance;
@@ -19,5 +24,34 @@ describe('SearchAccountComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // search Account test
+
+  it('should send a POST request for searchAccount', () => {
+    const email = 'ali@gmail.com';
+    const expectedResponse = {
+      success: true,
+      user: {
+        email: 'ali@gmail.com',
+      },
+    };
+    const fixture = TestBed.createComponent(SearchAccountComponent);
+    const component = fixture.componentInstance;
+    const service = fixture.debugElement.injector.get(AuthService);
+    spyOn(service, 'searchAccount').and.returnValue(of(expectedResponse));
+    const setUserInfosSpy = spyOn(component.setUserInfos, 'emit');
+    const setVisibleSpy = spyOn(component.setVisible, 'emit');
+
+    component.searchForm.get('email')?.setValue(email);
+    component.handleSearch();
+
+    expect(component.setLoading).toBe(false);
+    expect(component.setError).toBe('');
+
+    expect(setUserInfosSpy).toHaveBeenCalledWith(expectedResponse);
+    expect(setVisibleSpy).toHaveBeenCalledWith(1);
+
+    expect(service.searchAccount).toHaveBeenCalledWith(email);
   });
 });
